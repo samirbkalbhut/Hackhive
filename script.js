bf6387095e4e77796fd04927a8a41719b4974ea4
samirbkalbhut <148427063+samirbkalbhut@users.noreply.github.com>
2025-08-06 09:16:54 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function showSection(sectionId) {
-    // Hide all sections
-    document.querySelectorAll('section').forEach(section => {
-        section.style.display = 'none';
-    });
-
-    // Show the selected section
-    document.getElementById(sectionId).style.display = 'block';
-}
